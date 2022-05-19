d2edf8beb71a4b635644ec6642780655178852a1
Colonel_Kristoffy <liam@kristoffy.com>
2022-05-19 10:07:09 +0100

    New file for funciton that finds MSE of two images.

---
+++ compare_img.py
@@ -0,0 +1,29 @@
+
+from ct_scan import *
+from ct_calibrate import *
+from ct_lib import *
+from ramp_filter import *
+from back_project import *
+from hu import *
+
+
+
+
+def compare_im(real_im, recon_im, threshold):
+	if real_im.shape != recon_im.shape:
+		raise ValueError('Reconstruted image must be the same size as the origional image')
+	else:
+		#Normalise arrays first
+		real_im = real_im / np.linalg.norm(real_im)
+		recon_im = recon_im / np.linalg.norm(recon_im)
+
+		#Find mean squared error
+		error_im = np.subtract(real_im,recon_im)
+		draw(error_im)
+		MSE = (np.square(error_im)).mean()
+		#Test if above threshold
+		if MSE > threshold:
+			print(MSE)
+			raise ValueError('Mean squared error is above threshold')
+		else:
+			print("Test Passed. MSE=", MSE)
